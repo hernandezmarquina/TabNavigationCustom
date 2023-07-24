db78f33f6807d437b31c07e556b1bd3f7b0732cc
Jonathan Hernandez <jonathan.hernandez@bowheadhealth.com>
2023-07-23 20:16:01 -0600

    Update assets

---
+++ assets/index.ts
@@ -0,0 +1,19 @@
+const chat = require('./chat.png');
+const chatWhite = require('./chat-white.png');
+const user = require('./user.png');
+const userWhite = require('./user-white.png');
+const map = require('./maps.png');
+const mapWhite = require('./maps-white.png');
+const settings = require('./settings.png');
+const settingsWhite = require('./settings-white.png');
+
+export {
+  chat,
+  chatWhite,
+  user,
+  userWhite,
+  map,
+  mapWhite,
+  settings,
+  settingsWhite,
+};
